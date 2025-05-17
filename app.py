6e62fa2a7515445a644a471c4a9c2242471a0640
kentaroabe1109 <kentaroabe1109@gmail.com>
2025-05-17 21:48:31 +0900

    test

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hello, World!'
+  
+    load_dotenv()  # .env を読み込む
+
+    api_key = os.getenv("test_key")
+    debug_mode = os.getenv("DEBUG") == "True"
+    return api_key
